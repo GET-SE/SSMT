4e3bed7db179d4ea418300c7a0fdd9656d70d531
KusumanadhK <kusumanadhkatru.n170953@msitprogram.net>
2023-11-01 18:35:04 +0530

    added new branch with deployment parameters

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import "../styles/globals.css"; // The path to your global CSS file
+import "../styles/global.css"; // The path to your global CSS file
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
